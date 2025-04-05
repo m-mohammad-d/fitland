4f2bb12a56c442dbca7f6ffe712362f94daf2eb3
m-mohammad-d <silvershade1000@gmail.com>
2025-04-05 18:28:28 +0330

    feat : add loading spinner

---
+++ src/components/ui/DotSpinner.tsx
@@ -0,0 +1,9 @@
+function DotSpinner() {
+  return (
+    <div className="fullscreen-spinner">
+      <div className="DotSpinner"></div>
+    </div>
+  );
+}
+
+export default DotSpinner;
---
+++ src/components/ui/UploadSpinner.tsx
@@ -0,0 +1,5 @@
+function UploadSpinner() {
+  return <span className="upload-spinner"></span>;
+}
+
+export default UploadSpinner;
