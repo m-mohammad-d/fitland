9fdb57d4ad5cbb73b8d3a6b3c04d0f960043af6b
m-mohammad-d <silvershade1000@gmail.com>
2025-04-28 20:45:04 +0330

    feat : add walletSuccess page

---
+++ src/app/(userPanel)/account/wallet/success/page.tsx
@@ -0,0 +1,12 @@
+import Celebration from "@/components/ui/Celebration";
+
+export default function WalletSuccess() {
+  return (
+    <Celebration
+      title="🎉 ولت شما شارژ شد!"
+      message="موجودی ولت شما با موفقیت افزایش یافت."
+      redirectText="رفتن به کیف پول"
+      redirectTo="/account/wallet"
+    />
+  );
+}
