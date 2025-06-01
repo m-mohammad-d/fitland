834abcc383eeff0bc9b504ac20007650eee35de2
m-mohammad-d <silvershade1000@gmail.com>
2025-06-01 17:36:18 +0330

    refactor: update middleware to only check token presence

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getUserFromTokenHeader } from "./lib/Auth";
-import { forbidden } from "next/navigation";
+
 export function middleware(req: NextRequest) {
   const url = req.nextUrl;
-  const cookieHeader = req.headers.get("cookie");
-  const user = getUserFromTokenHeader(cookieHeader);
-  if (url.pathname.startsWith("/account")) {
-    if (!user) {
+
+  if (url.pathname.startsWith("/account") || url.pathname.startsWith("/dashboard")) {
+    const token = req.cookies.get("auth-token");
+
+    if (!token) {
       return NextResponse.redirect(new URL("/login", req.url));
     }
   }
 
-  if (url.pathname.startsWith("/dashboard")) {
-    if (user?.role !== "ADMIN") {
-      return NextResponse.redirect(new URL("/forbidden", req.url));
-    }
-  }
   return NextResponse.next();
 }
 
