4fe367ac123a4890b87d3f3e3d33908adf0e0cfc
m-mohammad-d <silvershade1000@gmail.com>
2025-06-02 17:40:07 +0330

    responsive products grid

--- src/components/products/ProductGrid.tsx
+++ src/components/products/ProductGrid.tsx
@@ -7,9 +7,8 @@ interface ProductGridProps {
 }
 
 const ProductGrid: React.FC<ProductGridProps> = ({ products }) => {
-  console.log(products);
 
-  return <div className="mx-4 grid grid-cols-2 gap-6 md:grid-cols-3 xl:grid-cols-4">{products?.map((product) => <BestSallersProduct key={product.id} product={product} />)}</div>;
+  return <div className="mx-4 grid grid-cols-1 gap-6 place-items-center sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">{products?.map((product) => <BestSallersProduct key={product.id} product={product} />)}</div>;
 };
 
 export default ProductGrid;
